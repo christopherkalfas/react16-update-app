79e31737a1eb973455a273441df72142f5cbdcc5
Chris Kalfas <christopherkalfas22@gmail.com>
2020-02-04 14:02:14 -0500

    laid out app structure, taking a users name as input and rendering an error message using componentDidCatch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import StudentForm from './components/StudentForm';
+
+class App extends Component {
+
+  state = {
+    hasError: false
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true
+    })
+  }
+  
+  render() {
+    if(this.state.hasError) {
+      return(<h1>Oh no! Something went wrong.</h1>)
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <StudentForm />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
+
