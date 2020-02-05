d881c18a8caa7c89fdf2a9bdeb5c83aa43cbd26f
Chris Kalfas <christopherkalfas22@gmail.com>
2020-02-05 13:25:18 -0500

    Added a portal, a new feature of React 16. Portals allows rendering of child elements into the DOM that are OUTSIDE of the App DOM tree.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
 
 import StudentForm from './components/StudentForm';
 import ErrorBoundary from './components/ErrorBoundary'
+import Modal from './components/Modal'
 
 class App extends Component {
+
+  handleClick = () => {
+    console.log('I was clicked')
+  }
   
   render() {
       return (
-        <div className="App">
+        <div className="App" onClick={this.handleClick}>
+
           <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -17,6 +24,12 @@ class App extends Component {
           <ErrorBoundary>
             <StudentForm />
           </ErrorBoundary>
+          <Modal>
+            <div className='modal'>
+              This is a Modal Window
+              <button>Close</button>
+            </div>
+          </Modal>
         </div>
       );
   }
---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+
+class Modal extends Component {
+    render() {
+        return(
+            ReactDOM.createPortal(
+                this.props.children,
+                document.getElementById('my-portal')
+            )
+        )
+    }
+}
+
+export default Modal
